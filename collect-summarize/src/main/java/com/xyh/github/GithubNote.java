d55a119e67adf43c9f5acdfc4d3fbbd8c18e9f8c
hcxyh <172977359@qq.com>
2018-08-14 17:34:07 +0800

    :rocket:Road from here, there is no end.

--- collect-summarize/src/main/java/com/xyh/github/GithubNote.java
+++ collect-summarize/src/main/java/com/xyh/github/GithubNote.java
@@ -11,23 +11,53 @@ public class GithubNote {
 	 * github上start了很多项目,一直都是零散的clone下来学习,没有归集分类.
 	 */
 	/**
+	 awesome
+	  	1.https://github.com/sindresorhus/awesome
+	 
 	 1.tool
 	 	1.下载
 	 		proxyee-down ： https://github.com/proxyee-down-org/proxyee-down
 	 	2.
 	 2.教程总结集合
-	 	1.tutorials 	：	 https://github.com/eugenp/tutorials
+	 	1.tutorials 
+	 		https://github.com/eugenp/tutorials
 	 	2.电子书集合
 	 		https://github.com/justjavac/free-programming-books-zh_CN
-	 	3.	
+	 		https://github.com/EbookFoundation/free-programming-books/blob/master/free-programming-books-zh.md
+	 	3.伯乐在线资料大全
+	 		https://github.com/jobbole
+	 		https://github.com/jobbole/awesome-java-cn
+	 	4.w3cschool
+	 		https://github.com/it-ebooks/w3school
+	 3.工程代码
+	 	1.pay
+	 		xxx-pay 
+	 			https://github.com/jmdhappy/xxpay-master
+	 		fast-pay
+	 		roncoo-pay
+	 	2.b2b购物商场
+	 		1.vue+springcloue
+	 			https://github.com/paascloud/paascloud-master
+	 	3.id生成
+	 		1.leaf
+	 			https://github.com/Yaccc/leafserver
+	 		2.snowflake	
+	 			https://github.com/beyondfengyu/DistributedID
+	 	4.netty
+	 		1.netty-chat
+	 			https://github.com/beyondfengyu/HappyChat
+	 			
+	 			
+	 			
 	 */
 	
 	/**
 	  awesome系列:
 	  	Java资源大全
 		  	1.https://github.com/jobbole/awesome-java-cn
-		  	2.
-		 
+			2.https://github.com/brianway/java-learning
+		前端知识汇总
+			1.https://github.com/Louiszhai/Louiszhai.github.io
 	 
 	 */
 }
--- collect-summarize/src/main/java/com/xyh/middleware/rpc/simple/package-info.java
+++ collect-summarize/src/main/java/com/xyh/middleware/rpc/simple/package-info.java
@@ -10,6 +10,7 @@ package com.xyh.middleware.rpc.simple;
 /**
  * 1.https://github.com/hank-whu/turbo-rpc
  * 	 https://github.com/yangzhenkun/krpc
+ * 	 https://github.com/tang-jie/NettyRPC
  * 
  * 2.自己实现的nettyPrc
  * 
