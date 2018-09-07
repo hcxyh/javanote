a1a60b69e58219d781c8a13237ea5a07ac0ab620
hcxyh <172977359@qq.com>
2018-09-07 15:43:21 +0800

    syncCode

---
+++ collect-summarize/src/main/java/com/xyh/java/jdk8/CompletableFuture.java
@@ -0,0 +1,11 @@
+package com.xyh.java.jdk8;
+
+/**
+ * @author xyh
+ *
+ */
+public class CompletableFuture {
+	
+	
+
+}
--- collect-summarize/src/main/java/com/xyh/java/jdk8/package-info.java
+++ collect-summarize/src/main/java/com/xyh/java/jdk8/package-info.java
@@ -6,6 +6,7 @@
  * 1.lambda
  * 2.Stream流式处理
  * 3.函数式编程
+ * 4.CompletableFuture 异步调用 , 区别于future，在guava中有实现.
  *
  */
 package com.xyh.java.jdk8;
